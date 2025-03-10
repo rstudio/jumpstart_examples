f44955ee0438b7563c1887efaba769257f48b62f
Ian Pittwood <ian.pittwood@rstudio.com>
2025-03-10 12:04:36 -0600

    Change output to pdf

--- bundles/generate-report-pdf-shiny/app.R
+++ bundles/generate-report-pdf-shiny/app.R
@@ -6,7 +6,7 @@ shinyApp(
   server = function(input, output) {
     output$report <- downloadHandler(
       # For PDF output, change this to "report.pdf"
-      filename = "report.html",
+      filename = "report.pdf",
       content = function(file) {
         # Copy the report file to a temporary directory before processing it, in
         # case we don't have write permissions to the current working dir (which
